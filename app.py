2353ae2ee33b833cdee4d2aeb9af652403c0f349
Kevin Ng <80858693+kn2118@users.noreply.github.com>
2023-04-09 15:06:28 -0400

    beautified output and implemented default input

--- app.py
+++ app.py
@@ -13,5 +13,10 @@ classifier = pipeline("sentiment-analysis")
 # text input 
 user_input = st.text_input("Enter your text here.")
 
+if user_input == "":
+    user_input = "I am amazing!"
+
 if st.button('Submit'):
-    st.write(classifier(user_input))
+    st.write("Input: ", user_input)
+    st.write("Label: ", classifier(user_input)[0]["label"])
+    st.write("Accuracy: ", classifier(user_input)[0]["score"])
