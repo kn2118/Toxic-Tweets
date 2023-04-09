228fc8c871b91de1c1806b3e37fd959b527364f2
Kevin Ng <80858693+kn2118@users.noreply.github.com>
2023-04-09 14:56:05 -0400

    user input -> sentiment analysis barebones; no model selection feature yet

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from transformers import pipeline 
+import streamlit as st 
 
+# title 
+st.title("Toxic Tweets") 
+
+# subtitle 
+st.markdown("Link to the app- ")
+
+# define model 
+classifier = pipeline("sentiment-analysis")
+
+# text input 
+user_input = st.text_input("Enter your text here.")
+
+if st.button('Submit'):
+    st.write(classifier(user_input))
